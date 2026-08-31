016380f859d4e26f95075a86da691ff9c12e6ef6
Madeleine Johnson (S) <m_johnson2023@login.mcscn.cs.coloradocollege.edu>
2026-08-31 15:50:56 -0600

    initial agent word

--- agent.c
+++ agent.c
@@ -1,3 +1,57 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
 //this will hold our agent structs
+//
+
+#define NAGENTS 20
+
+typedef struct {
+	int id;
+	int currentNode;
+	bool isInfected;
+	bool disposition;
+
+}Agent;
+
+//if agent interacts with 3 other infected agents- infected
+//
+
+
+int main() {
+	
+
+	//memory for list of agents
+	Agent* agents = malloc(NAGENTS * sizeof(Agent));
+	float numInfected = NAGENTS*0.05;
+	
+	//counter to see how many have a disposition already
+	numAddict = 0;
+
+	//10% of americans are addicted to drugs. 5% of ours will start addicted
+	for(int i = 0; i<NAGENTS; i++){
+		if(i< numInfected){
+			agents[i].isInfected = true;
+		}
+		agents[i].id = i;
+		//gotta make it deterministic??!!
+		//also gotta make it random because 
+		//agents[i].currentNode = ;
+		
+		//maybe like 20% are moderately prone to addiction. 20 are heavily prone and 60 are just average
+		int randomInfect = rand(); % 21;
+		if(randomInfect > 16 && numAddict < 4){
+			numAddict ++;
+			agents[i].disposition = 
+			
+		}
+		agents[i].disposition
+		
+
+	}
+	
+
+	return 0;
+}
