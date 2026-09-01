f84846b6056334b84f03b960780e6007dda1a42d
Madeleine Johnson (S) <m_johnson2023@login.mcscn.cs.coloradocollege.edu>
2026-09-01 13:58:38 -0600

    header file and simulate we will need a main

---
+++ agent.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+//this will hold our agent structs
+//
+
+#define NAGENTS 20
+
+typedef struct {
+	int id;
+	int homeNode;
+	int currentNode;
+	bool isInfected;
+	bool disposition;
+	//using a fixed size array for this next one because we know it's max... would changing the memory later on be faster?
+	int encounters[NAGENTS];
+
+}Agent;
+
+//if agent interacts with 3 other infected agents- infected
+//
+
+void wakeAgent();
+
+Agent*  makeAgent(int NAGENTS, struct Graph* graph) ;
--- simulate.c
+++ simulate.c
@@ -1,2 +1,53 @@
 
-//will go through the world updating agents
+//will go through the world updating agent
+//
+//
+
+void updateInfection(Agent* agents, int numAgents){
+
+	//want to go through, check how many infection counters an agent had
+	//I actually don't think that an agent shoudl become infected in the middle of the day... infections are gradual?? 
+	
+	for (int i = 0; i < numAgents; i++) {
+        	if (agents[i].isInfected) continue;
+
+		int infectedContacts = 0;
+        	for (int j = 0; j < numAgents; j++) {
+            		if (agents[i].encounters[j] > 0 && agents[j].isInfected) {
+                		infectedContacts++;
+            		}
+        	}
+        	if (infectedContacts >= 3) {
+            		agents[i].isInfected = true;
+        	}
+
+
+	}
+
+}
+
+
+int main(){
+
+
+	struct Graph* graph = createGraph(/* however many nodes */);
+	//making struct in here?? adding edge???
+
+	//maybe not the right place / way to be doing this come back to it
+	Agent* agents = makeAgents(NAGENTS, graph);
+	
+	if (agents == NULL) {
+        	fprintf(stderr, "Failed to allocate agents\n");
+        	return 1;
+    	}
+
+	//eventually need to free everything
+	//free(agents);
+	//free(graph->nodes);
+	//free(graph);
+
+	return 0;
+
+
+
+}
