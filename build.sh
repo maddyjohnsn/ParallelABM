4ad153e8c39314da9bc71915df45da77c4abed8e
Charlie Melamede (S) <c_melamede2023@coloradocollege.edu>
2026-09-13 17:29:25 -0600

    merging

--- build.sh
+++ build.sh
@@ -24,11 +24,20 @@ gcc -fopenmp mainBaseDays6.c state.c agent.c simulatePar5.c -o parallel_v5
 # parallel v6 
 gcc -fopenmp mainBaseDays6.c state.c agent.c simulatePar6.c -o parallel_v6
 
+# parallel v6 RNG Change
+gcc -fopenmp mainBaseDays6.c state.c agent.c simulatePar6RNGChange.c -o parallel_v6_RNGChange
+
 # parallell v7 (parallel 6 with struct reorg)
 gcc -fopenmp mainBaseDays_v2.c state_v2.c agent_v2.c simulatePar6_v2.c -o parallel_v7
 
 # parallell v8 (parallel 6 with struct reorg and -O3 compiler flag)
 gcc -fopenmp -O3 mainBaseDays_v2.c state_v2.c agent_v2.c simulatePar6_v2.c -o parallel_v8
+
+#parallel v9 (parv6 with RNG Change, compiler flags, and struct reorg)
+gcc -fopenmp -O3 mainBaseDays_v2.c state_v2.c agent_v2.c simulatePar6RNGChange_v2.c -o parallel_v9
+
+# parallel v10 RNG Change with compiler flag
+gcc -fopenmp -O3 mainBaseDays6.c state.c agent.c simulatePar6RNGChange.c -o parallel_v10
 
 # initial serial -03
 gcc -O3 mainBaseDays.c state.c agent.c simulate.c -o serial_v1_O3
